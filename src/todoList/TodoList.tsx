bb4a1c4f78f72fc5886cb1c83ff787153de049bb
Marco Agostinelli <51815533+marcoMilkman@users.noreply.github.com>
2021-08-17 15:51:58 +0200

    test indentation with spaces

--- src/todoList/TodoList.tsx
+++ src/todoList/TodoList.tsx
@@ -8,44 +8,44 @@ import Status from "./constants/Status";
 const list = new TodoListStore({});
 
 const TodoList = observer(() => {
-	const [value, setValue] = useState<string>('')
+  const [value, setValue] = useState<string>('')
 
-	// Actions
-	const handleChange = (e: { target: { value: string } }) => {
-		const { target: { value } } = e || {}
-		setValue(value)
-	}
+  // Actions
+  const handleChange = (e: { target: { value: string } }) => {
+    const { target: { value } } = e || {}
+    setValue(value)
+  }
 
-	const handleClick = () => {
-		const itemToSave = new TodoItemStore(value)
-		list.setItems(itemToSave)
-		setValue('')
-	}
+  const handleClick = () => {
+    const itemToSave = new TodoItemStore(value)
+    list.setItems(itemToSave)
+    setValue('')
+  }
 
-	// Render
-	const renderItem = (item: Item) => (
-		<div>
-			<TodoItem
-				key={item.value}
-				item={item}
-			/>
-		</div>
-	)
+  // Render
+  const renderItem = (item: Item) => (
+    <div>
+      <TodoItem
+        key={item.value}
+        item={item}
+      />
+    </div>
+  )
 
-	return (
-		<div>
-			<div>
-				<button onClick={() => list.setFilterKey('')}>All</button>
-				<button onClick={() => list.setFilterKey(Status.TODO)}>To do</button>
-				<button onClick={() => list.setFilterKey(Status.COMPLETED)}>Completed</button>
-			</div>
-			<input value={value} onChange={handleChange} />
-			<button disabled={!value.length} onClick={handleClick}>Add</button>
-			<div>
-				{list.filteredItems.map(renderItem)}
-			</div>
-		</div>
-	)
+  return (
+    <div>
+      <div>
+        <button onClick={() => list.setFilterKey('')}>All</button>
+        <button onClick={() => list.setFilterKey(Status.TODO)}>To do</button>
+        <button onClick={() => list.setFilterKey(Status.COMPLETED)}>Completed</button>
+      </div>
+      <input value={value} onChange={handleChange} />
+      <button disabled={!value.length} onClick={handleClick}>Add</button>
+      <div>
+        {list.filteredItems.map(renderItem)}
+      </div>
+    </div>
+  )
 })
 
 export default TodoList
